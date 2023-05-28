df5e01489eb5c23496fe69459b46493c5875265d
Gabriel Magioli <gabrielmagioli1@gmail.com>
2023-05-28 15:57:43 -0300

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,31 @@
+import "./header.css";
+
+const Header = () => {
+    <>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet"/>
+    </>
+
+
+    return(
+        <>
+        <header className="header">
+            <div>
+            <img src="https://bitbucket.org/repo/ydBdKa/images/3020985987-Stream%20Logo.png" alt="logo website" className="logo"/>
+            </div>
+            <div>
+                <nav className="pastas-navegacao">
+                    <h2 className="pasta">MAIN</h2>
+                    <h2 className="pasta">NEWS</h2>
+                    <h2 className="pasta">CONTACT</h2>
+                    <h2 className="pasta">YOUR ACCOUNT</h2>
+                    <h2 className="pasta">SIGN OUT</h2>   
+                </nav>
+            </div>
+        </header>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/indicacaoFilme/IndicacaoFilme.js
@@ -0,0 +1,12 @@
+import "./indicacao.css";
+
+function IndicacaoFilme () {
+    return(
+        <section className="indicacaoFilme">
+            <img src="https://is4-ssl.mzstatic.com/image/thumb/Video113/v4/ee/d5/27/eed5274f-1496-2269-e5f0-ff62bfd10f42/source/900x900bb.jpg" alt="filme-indicado" className="filmeIndicado"/>
+        </section>
+    );
+}
+
+export default IndicacaoFilme;
+
